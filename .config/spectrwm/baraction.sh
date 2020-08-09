1f469ebdb1902423c88fa7065cecbee868d7bc63
dst80 <kleinerbastler80@gmail.com>
2020-08-09 10:04:36 +0200

    change baractions.sh and some settings in spectrewm

--- .config/spectrwm/baraction.sh
+++ .config/spectrwm/baraction.sh
@@ -3,13 +3,13 @@
 
 ## DISK
 hdd() {
-  hdd="$(df -h | awk 'NR==4{print $3, $5}')"
+  hdd="$(df -h | awk 'NR==4{printf "%.2fG %d %", $3, $5}')"
   echo -e "HDD: $hdd"
 }
 
 ## RAM
 mem() {
-  mem=`free | awk '/Mem/ {printf "%dM/%dM\n", $3 / 1024.0, $2 / 1024.0 }'`
+  mem=`free | awk '/Mem/ {printf "%.3fG/%.3fG\n", $3 / 1024.0 / 1024.0, $2 / 1024.0 / 1024.0 }'`
   echo -e "$mem"
 }
 
@@ -35,9 +35,10 @@ SLEEP_SEC=3
 
 # It seems that we are limited to how many characters can be displayed via
 # the baraction script output. And the the markup tags count in that limit.
-# So I would love to add more functions to this script but it makes the 
+# So I would love to add more functions to this script but it makes the
 # echo output too long to display correctly.
 while :; do
-    echo "+@fg=1; +@fn=1;💻+@fn=0; $(cpu) +@fg=0; | +@fg=2; +@fn=1;💾+@fn=0; $(mem) +@fg=0; | +@fg=3; +@fn=1;💿+@fn=0; $(hdd) +@fg=0; | +@fg=4; +@fn=1;🔈+@fn=0; $(vol) +@fg=0; |"
+    echo "+@fg=1;+@fn=0; $(cpu)+@fg=0; |+@fg=2;+@fn=0; $(mem)+@fg=0; |+@fg=3;+@fn=0; $(hdd)+@fg=0; |+@fg=4;+@fn=0;$(vol)+@fg=0; |"
+    l
 	sleep $SLEEP_SEC
 done
